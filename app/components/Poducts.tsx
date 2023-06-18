0da9639a0d8276a0e8f7a7cbbc47521fe0dd250d
Esin Andrew <kubanesin@gmail.com>
2023-06-18 11:08:50 +0300

    feat: сделал небольшой проект на Nextjs

---
+++ app/components/Poducts.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import ProductImage from './ProductImage';
+
+
+
+type Props = {
+  product: Product;
+};
+
+const Poduct = ({ product }: Props) => {
+  return (
+    <Link
+      className="h-96 flex flex-col border group-hover:scale-105 transition-transform ease-out duration-200"
+      prefetch={false}
+      href={`/product/${product.id}`}>
+      <div className="relative max-h-72 flex-1">
+        <ProductImage product={product} fill />
+      </div>
+      <div className="font-semibold flex items-center justify-between mt-4 mb-1">
+        <p>{product.title}</p>
+        <p>${product.price}</p>
+        <p className="italic text-xs w-64 line-clamp-2 text-gray-600">{product.description}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Poduct;
